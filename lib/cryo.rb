0f0035f5a8526784d3c54935a9742ac3d0da5223
Pierre Carrier <pierre@gcarrier.fr>
2013-08-16 13:19:52 +0200

    introduce report path

--- lib/cryo.rb
+++ lib/cryo.rb
@@ -1,5 +1,6 @@
 require 'aws-sdk'
 require 'logger'
+require 'json'
 
 ## require all ruby files recursively
 Dir.glob(File.join(File.dirname(__FILE__),'**/*.rb')).sort.each do |file|
@@ -15,12 +16,9 @@ class Cryo
     self.logger  = Logger.new(STDERR)
     logger.level = Logger::DEBUG
 
-    @time ||= get_utc_time  # don't change the endpoint!!!
-    @timestamp ||= @time.strftime('%Y/%m/%d/%H:%M:%S')
-
     @database = Database.create(options) \
       unless options[:type] == 'list' or options[:type] == 'get'
-    @store              = Store.create(options.merge(type: 's3', time: @time))
+    @store              = Store.create(options.merge(type: 's3', time: @start_time))
     @message            = Message.create(options.merge(type: 'sns'))
     @snapshot_prefix    = options[:snapshot_prefix]
     @archive_prefix     = options[:archive_prefix]
@@ -31,33 +29,63 @@ class Cryo
     @archive_bucket     = options[:archive_bucket]
     @tmp_path           = options[:tmp_path]
     @report_path        = options[:report_path]
-    @key                = "#{@snapshot_prefix}#{@timestamp}Z.cryo"
+    @key                = "#{@snapshot_prefix}#{@start_timestamp}Z.cryo"
+
+    @uncompressed_time, @backed_up_time, @stored_time = nil
+    @start_time ||= get_utc_time
+    @start_timestamp ||= @start_time.strftime('%Y/%m/%d/%H:%M:%S')
+
+    @uncompressed_size, @compressed_size = nil
   end
 
   def backup!
-    report = {}
-
     if @database.respond_to? 'get_gzipped_backup'
       logger.info 'getting compressed backup'
       compressed_backup = @database.get_gzipped_backup
-      logger.info "got backup in #{(get_utc_time - @time).round 2} seconds"
+      @compressed_size = File.size compressed_backup
+      @backed_up_time = get_utc_time
+      logger.info "got compressed backup in #{(@backed_up_time - @start_time).round 2} seconds"
     else
       logger.info 'taking backup...'
       backup_file = @database.get_backup
-      logger.info "got backup in #{(get_utc_time - @time).round 2} seconds"
+      @uncompressed_size = File.size backup_file
+      @uncompressed_time =  
+      logger.info "got backup in #{(get_utc_time - @start_time).round 2} seconds"
 
-      timer = get_utc_time
       logger.info 'compressing backup...'
       compressed_backup = gzip_file backup_file
-      logger.info "compressed backup in #{(get_utc_time - timer).round 2} seconds"
+      @compressed_size = File.size compressed_backup
+      @backed_up_time = get_utc_time
+      logger.info "compressed backup in #{(@backed_up_time - @uncompressed_time).round 2} seconds"
     end
 
-    timer = get_utc_time
     logger.info 'storing backup...'
-    @store.put(content: Pathname.new(compressed_backup), bucket: options[:snapshot_bucket],key: @key)
-    logger.info "upload took #{(get_utc_time - timer).round 2} seconds"
+    @store.put(
+      content: Pathname.new(compressed_backup),
+      bucket: options[:snapshot_bucket],
+      key: @key
+    )
+    @stored_time = get_utc_time
+    logger.info "upload took #{(@stored_time - @backed_up_time).round 2} seconds"
 
-    logger.info "completed entire backup in #{(get_utc_time - @time).round 2} seconds :)"
+    logger.info "completed entire backup in #{(@stored_time - @start_time).round 2} seconds :)"
+  end
+
+  def report
+    {
+      start_time:        @start_time.to_i,
+      uncompressed_time: @uncompressed_time.to_i,
+      backed_up_time:    @backed_up_time.to_i,
+      stored_time:       @stored_time.to_i,
+      compressed_size:   @compressed_size,
+      uncompressed_size: @uncompressed_size
+    }.delete_if do |k, v|
+      v == 0 and k.to_s.end_with? '_time'
+    end.to_json
+  end
+
+  def write_report
+    IO::write @report_path, report
   end
 
   def archive_and_purge
