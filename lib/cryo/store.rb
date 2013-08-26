1f9d7a6308330a5c668a59c7b5c6a345cfeb2a62
Pierre Carrier <pierre@gcarrier.fr>
2013-08-26 19:19:32 +0200

    remove tons of dead code
    We might reintroduce some, but this makes cryo a super small backup
    tool we can fully review carefully now.

--- lib/cryo/store.rb
+++ lib/cryo/store.rb
@@ -8,23 +8,11 @@ class Store
     self.logger = Logger.new(STDERR)
     logger.level = Logger::DEBUG
 
-    @snapshot_frequency = opts[:snapshot_frequency]
-    @archive_frequency  = opts[:archive_frequency]
-    @snapshot_period    = opts[:snapshot_period]
     @snapshot_prefix    = opts[:snapshot_prefix]
-    @archive_prefix     = opts[:archive_prefix]
     @time               = opts[:time]
   end
 
-  def get
-    raise NotImplementedError.new
-  end
-
   def put
-    raise NotImplementedError.new
-  end
-
-  def list_snapshots
     raise NotImplementedError.new
   end
 
@@ -32,80 +20,5 @@ class Store
     def create(options={})
       const_get(options[:type].to_s.capitalize).new(options)
     end
-  end
-
-  # TODO(pierre): reimplement
-  def recursive_archive_and_purge(opts={})
-    logger.debug 'entering recursive_archive_and_purge'
-    snapshot_list = opts[:snapshot_list]
-
-    # return if there are no snapshots
-    if snapshot_list.empty?
-      logger.info 'no snapshots found'
-      return true
-    end
-
-    # return if there are not enough snapshots avilable
-    minium_number_of_snapshots = (@snapshot_period.to_f/@snapshot_frequency.to_f).ceil
-    if snapshot_list.size < minium_number_of_snapshots
-      logger.info 'not enough snapshots avilable for archiving'
-      logger.info "we found #{snapshot_list.size} but we need to keep at least #{minium_number_of_snapshots}"
-      return true
-    end
-
-    oldest_snapshot = snapshot_list.shift
-    oldest_snapshot_age = get_age_from_key_name(oldest_snapshot)
-
-    logger.debug "oldest_snapshot is #{oldest_snapshot}"
-    logger.debug "oldest_snapshot_age is #{oldest_snapshot_age}"
-
-    # return if the oldest snapshot it not old enough to be archived
-    if oldest_snapshot_age < @snapshot_period
-      logger.info 'all snapshots are younger than snapshot_period'
-      return true
-    end
-
-    # if we got this far, then the oldest snapshot needs to be either archived or deleted
-    newest_archive = get_newest_archive
-
-    # check to see if we have any archives
-    if newest_archive.empty?
-      logger.info 'looks like we don\'t have any archives yet'
-      logger.info "archiving oldest snapshot #{oldest_snapshot}"
-      archive_snapshot oldest_snapshot
-      logger.debug 'recursing...'
-      recursive_archive_and_purge(snapshot_list: snapshot_list, newest_archive: oldest_snapshot)
-      return true
-    end
-
-    newest_archive_age = get_age_from_key_name(newest_archive)
-
-    # check to see if the oldest snapshot should be archived
-    if need_to_archive?(oldest_snapshot_age,newest_archive_age)
-      logger.info "archiving oldest snapshot #{oldest_snapshot}"
-      archive_snapshot oldest_snapshot
-      logger.debug 'recursing...'
-      recursive_archive_and_purge(snapshot_list: snapshot_list, newest_archive: oldest_snapshot)
-      return true
-    end
-
-    # check the next oldest snapshot too, before we throw this one away
-    second_oldest_snapshot = opts[:snapshot_list].first
-    second_oldest_snapshot_age = get_age_from_key_name(second_oldest_snapshot)
-
-    if need_to_archive?(second_oldest_snapshot_age,newest_archive_age)
-      logger.info "archiving oldest snapshot #{oldest_snapshot}"
-      archive_snapshot oldest_snapshot
-      logger.debug 'recursing...'
-      recursive_archive_and_purge(snapshot_list: snapshot_list, newest_archive: oldest_snapshot)
-      return true
-    end
-
-    # if we got this far, then we just need to delete the oldest snapshot
-    logger.info "deleting oldest snapshot #{oldest_snapshot}"
-    delete_snapshot oldest_snapshot
-    logger.debug 'recursing'
-    recursive_archive_and_purge(snapshot_list: snapshot_list, newest_archive: newest_archive)
-    true
   end
 end
--- lib/cryo/store/s3.rb
+++ lib/cryo/store/s3.rb
@@ -9,21 +9,6 @@ class S3 < Store
                :secret_access_key => opts[:aws_secret_key])
     @s3 = AWS::S3.new
     @snapshot_bucket = @s3.buckets[opts[:snapshot_bucket]]
-    @archive_bucket  = @s3.buckets[opts[:archive_bucket]]
-  end
-
-  def get(opts={})
-    bucket    = opts[:bucket]
-    key       = opts[:key]
-    file_path = opts[:file] || opts[:path]
-    if file_path
-      File.open(file_path,'w') do |file|
-        @s3.buckets[bucket].objects[key].read {|chunk| file.write chunk}
-        return true
-      end
-    else
-      @s3.buckets[bucket].objects[key].read
-    end
   end
 
   def put(opts={})
@@ -33,29 +18,8 @@ class S3 < Store
     @s3.buckets[bucket].objects[key].write(content) # TODO: verify that bucket exists?
   end
 
-  def etag(opts={})
-    bucket = opts[:bucket]
-    key    = opts[:key]
-    @s3.buckets[bucket].objects[key].etag
-  end
-
   # return an array listing the objects in our snapshot bucket
   def list_snapshots
     @s3.buckets[@snapshot_bucket].objects.with_prefix(@prefix).to_a
-  end
-
-  protected
-
-  def expand_snapshot_name(shortname)
-    @snapshot_prefix + shortname + 'Z.cryo'
-  end
-
-  def trim_snapshot_name(longname)
-    longname.gsub(/^#{@snapshot_prefix}/,'').gsub(/Z\.cryo$/,'')
-  end
-
-  def delete_snapshot(snapshot)
-    full_snapshot_name = expand_snapshot_name(snapshot)
-    @snapshot_bucket.objects[full_snapshot_name].delete
   end
 end
