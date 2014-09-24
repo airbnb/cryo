876a1c08e4fe94e1f0b9955dc4f4413f9af86c5b
Davide Cerri <davide.cerri@airbnb.com>
2014-09-24 16:43:55 -0700

    fix tarring of dirs

--- lib/cryo/database/filesystem.rb
+++ lib/cryo/database/filesystem.rb
@@ -10,14 +10,14 @@ class Filesystem < Database
   ## get a copy of the db from remote host
   def get_backup
     tar_file = get_tempfile
-    safe_run "tar -cf #{tar_file} #{tar_dir}/*"
+    safe_run "tar -cf #{tar_file} #{tar_dir}"
     tar_file
   end
 
   ## get a zipped copy of the db from remote host
   def get_gzipped_backup
     tar_file = get_tempfile
-    safe_run "tar -czf #{tar_file} #{tar_dir}/*"
+    safe_run "tar -czf #{tar_file} #{tar_dir}"
     tar_file
   end
 
